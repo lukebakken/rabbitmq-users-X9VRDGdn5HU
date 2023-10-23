1e12b4bc73a8c713898eff3e7245840cc4cca1e0
Luke Bakken <luke@bakken.io>
2023-10-23 12:17:01 -0700

    rename

--- uninstall.ps1
+++ uninstall.ps1
@@ -7,9 +7,9 @@ Set-StrictMode -Version 2.0
 New-Variable -Name curdir  -Option Constant -Value $PSScriptRoot
 Write-Host "[INFO] curdir: $curdir"
 
-New-Variable -Name opt_dir  -Option Constant -Value (Join-Path -Path $curdir -ChildPath 'opt')
-New-Variable -Name otp_25_dir  -Option Constant -Value (Join-Path -Path $opt_dir -ChildPath '25')
-New-Variable -Name otp_26_dir  -Option Constant -Value (Join-Path -Path $opt_dir -ChildPath '26')
+New-Variable -Name otp_dir  -Option Constant -Value (Join-Path -Path $curdir -ChildPath 'otp')
+New-Variable -Name otp_25_dir  -Option Constant -Value (Join-Path -Path $otp_dir -ChildPath '25')
+New-Variable -Name otp_26_dir  -Option Constant -Value (Join-Path -Path $otp_dir -ChildPath '26')
 
 New-Variable -Name otp_25_uninstall_exe  -Option Constant -Value (Join-Path -Path $otp_25_dir -ChildPath 'uninstall.exe')
 New-Variable -Name otp_26_uninstall_exe  -Option Constant -Value (Join-Path -Path $otp_26_dir -ChildPath 'uninstall.exe')
